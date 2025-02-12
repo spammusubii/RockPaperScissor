1c9253da4a949bde31b718c692954e6c247995fd
Justin <194769941+spammusubii@users.noreply.github.com>
2025-02-12 00:30:49 -0800

    Create a UI for Rock, Paper, Scissor Game Made rock, paper, scissor buttons so user can use buttons instead of typing their choice. Allows for games with differing number of rounds. Additionally created a reset button to use while playing a game.

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissor = document.querySelector('#scissor');
+const rpsButtons = [rock, paper, scissor];
+const result = document.querySelector('.result');
+const gameButton = document.querySelector('.game-button');
+let numberOfRounds = document.querySelector("#numberOfRounds").value;
+let numberOfRoundsPlayed = 0;
+let humanWins = 0;
+let computerWins = 0;
+const numberOfRoundsInput = document.querySelector("#numberOfRounds");
+
+gameButton.addEventListener('click', (e) => {
+    if (e.target.textContent === "Start") {
+        startGame();
+    } else {
+        resetGame();
+        changeInputAvailability();
+        if (!paper.disabled) {
+            changeRpsButtonsAvailability();
+        }
+    }
+})
+
+rpsButtons.forEach(button => button.addEventListener('click', (e) => {
+    const humanChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    if (numberOfRoundsPlayed == numberOfRounds){
+        changeRpsButtonsAvailability();
+        declareGameWinner(`${computerWins > humanWins ? 'computer' : computerWins === humanWins ? 'tie' : 'human'}`)
+    }
+}))
+
+// Disable/Enable Rock, Paper, Scissors buttons
+function changeRpsButtonsAvailability() {
+    const rpsList = rpsButtons.forEach(element=>element.disabled=!element.disabled);
+    return rpsList;
+}
+
+// Disable/Enable number of rounds input
+function changeInputAvailability() {
+    numberOfRoundsInput.disabled = !numberOfRoundsInput.disabled;
+}
+
 // declare function getComputerChoice
 function getComputerChoice() {
     // create variable result to contain computer's choice
@@ -29,43 +74,6 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// Declare function getHumanChoice
-function getHumanChoice(){
-
-    // Declare variable humanChoice as undefined
-    let humanChoice;
-
-    // use while loop to repeat until given a valid response or q is given
-    while (humanChoice === undefined){
-        // Use prompt to get human's choice
-        // Ask for rock, paper, or scissor and
-        // rock can be 1, paper can be 2, and scissor can be 3, or quit can be q
-        // store in response variable
-        let response = prompt("Rock (1), paper (2), or scissor (3)").toLowerCase();
-
-        // Use if/else if/else to see what the human chose
-        // IF response is equal to "1" or "rock"
-            // humanChoice = "rock"
-        if (response === "rock" || response === "1"){
-            humanChoice = "rock";
-        }
-        // ELSE IF response is equal to "2" or "paper"
-            // humanChoice = "paper"
-        else if (response === "paper" || response === "2"){
-            humanChoice = "paper";
-        }
-        // ELSE IF response is equal to "3" or "scissor"
-            // humanChoice = "scissor"
-        else if (response === "scissor" || response === "3"){
-            humanChoice = "scissor";
-        }
-        // repeat until correct choice is given or exit
-    }
-
-    // return humanChoice
-    return humanChoice;
-}
-
 // Declare function playRound with parameters humanChoice and computerChoice
 function playRound(humanChoice, computerChoice){
     let winner;
@@ -73,18 +81,18 @@ function playRound(humanChoice, computerChoice){
     if (humanChoice === "rock"){
         // if computerChoice is equal to paper, then computer wins
         if (computerChoice === "paper"){
-            computer += 1;
+            computerWins += 1;
             winner = "computer"
         }
         // else if computerChoice is equal to scissor, human wins
         else if (computerChoice === "scissor"){
-            human += 1;
+            humanWins += 1;
             winner = "human";
         }
         // else its a tie
         else {
-            human += 1;
-            computer += 1;
+            humanWins += 1;
+            computerWins += 1;
             winner = "tie";
         }
     }
@@ -92,18 +100,18 @@ function playRound(humanChoice, computerChoice){
     else if (humanChoice === "paper"){
         // if computerChoice is equal to scissor, then computer wins
         if (computerChoice === "scissor"){
-            computer += 1;
+            computerWins += 1;
             winner = "computer"
         }
         // if computerChoice is equal to rock, then human wins
         else if (humanChoice === "rock"){
-            human += 1;
+            humanWins += 1;
             winner = "human";
         }
         // else its a tie
         else {
-            human += 1;
-            computer += 1;
+            humanWins += 1;
+            computerWins += 1;
             winner = "tie";
         }
     }
@@ -111,148 +119,79 @@ function playRound(humanChoice, computerChoice){
     else {
         // if computerChoice is equal to rock, then computer wins
         if (computerChoice === "rock"){
-            computer += 1;
+            computerWins += 1;
             winner = "computer"
         }
         // if computerChoice is equal to paper, then human wins
         else if (computerChoice === "paper"){
-            human += 1;
+            humanWins += 1;
             winner = "human";
         }
         // else its a tie
         else {
-            human += 1;
-            computer += 1;
+            humanWins += 1;
+            computerWins += 1;
             winner = "tie";
         }
     }
 
-    // IF human is winner
+    ++numberOfRoundsPlayed;
+    declareRoundWinner(winner, humanChoice, computerChoice);
+}
+
+function declareRoundWinner(winner, humanChoice, computerChoice) {
+     // IF human is winner
     if (winner === "human"){
         // output you win, human's choice beats computer's choice
-        console.log(`You win! ${humanChoice.slice(0,1).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`)
+        result.innerHTML += `<p>Round ${numberOfRoundsPlayed}: You win! Your choice of ${humanChoice} beat the computer's choice of ${computerChoice}.</p>`
     }
     // ELSE IF computer is winner
     else if (winner === "computer"){
         // output you lose, computer's choice beats human's choice
-        console.log(`You lose! ${computerChoice.slice(0,1).toUpperCase() + computerChoice.slice(1)} beats ${human}.`)
+        result.innerHTML += `<p>Round ${numberOfRoundsPlayed}: You lose! The computer's choice of ${computerChoice} beat your choice of ${humanChoice}.</p>`
     }
     // ELSE its a tie
     else {
         // output tie, both you and the computer chose ###, so both of you get a point
-        console.log(`It's a tie! You both chose ${humanChoice}.`)
+        result.innerHTML += `<p>Round ${numberOfRoundsPlayed}: It's a tie! You both chose ${humanChoice}.</p>`
     }
 }
 
-// Declare playGame
-function playGame(){
-    // initialize human score to 0
-    let human = 0;
-
-    // initialize computer score to 0
-    let computer = 0;
-
-    // create function playRound
-    // Declare function playRound with parameters humanChoice and computerChoice
-    function playRound(humanChoice, computerChoice){
-        let winner;
-        // If humanChoice is equal to rock
-        if (humanChoice === "rock"){
-            // if computerChoice is equal to paper, then computer wins
-            if (computerChoice === "paper"){
-                computer += 1;
-                winner = "computer"
-            }
-            // else if computerChoice is equal to scissor, human wins
-            else if (computerChoice === "scissor"){
-                human += 1;
-                winner = "human";
-            }
-            // else its a tie
-            else {
-                human += 1;
-                computer += 1;
-                winner = "tie";
-            }
-        }
-        // ELSE IF humanChoice is equal to paper
-        else if (humanChoice === "paper"){
-            // if computerChoice is equal to scissor, then computer wins
-            if (computerChoice === "scissor"){
-                computer += 1;
-                winner = "computer"
-            }
-            // if computerChoice is equal to rock, then human wins
-            else if (humanChoice === "rock"){
-                human += 1;
-                winner = "human";
-            }
-            // else its a tie
-            else {
-                human += 1;
-                computer += 1;
-                winner = "tie";
-            }
-        }
-        // ELSE IF humanChoice is equal to scissor
-        else {
-            // if computerChoice is equal to rock, then computer wins
-            if (computerChoice === "rock"){
-                computer += 1;
-                winner = "computer"
-            }
-            // if computerChoice is equal to paper, then human wins
-            else if (computerChoice === "paper"){
-                human += 1;
-                winner = "human";
-            }
-            // else its a tie
-            else {
-                human += 1;
-                computer += 1;
-                winner = "tie";
-            }
-        }
-
-        // IF human is winner
-        if (winner === "human"){
-            // output you win, human's choice beats computer's choice
-            console.log(`You win! ${humanChoice.slice(0,1).toUpperCase() + humanChoice.slice(1)} beats ${computerChoice}.`)
-        }
-        // ELSE IF computer is winner
-        else if (winner === "computer"){
-            // output you lose, computer's choice beats human's choice
-            console.log(`You lose! ${computerChoice.slice(0,1).toUpperCase() + computerChoice.slice(1)} beats ${humanChoice}.`)
-        }
-        // ELSE its a tie
-        else {
-            // output tie, both you and the computer chose ###, so both of you get a point
-            console.log(`It's a tie! You both chose ${humanChoice}.`)
-        }
-    }
-
-    // use for loop to play 5 rounds
-    for (let i = 0; i < 5; i++){
-        const computerChoice = getComputerChoice();
-        const humanChoice = getHumanChoice();
-        console.log(`Round ${i+1}: You chose ${humanChoice} and Computer chose ${computerChoice}.`);
-        playRound(humanChoice, computerChoice);
-    }
-
-    // Output the winner of the game
-    // IF  human is greater than computer
-        // output that human won
-    if (human > computer){
-        console.log(`After 5 rounds, you won by a score of ${human} to ${computer}`);
+function declareGameWinner(winner) {
+    if (winner === "human"){
+        result.innerHTML += `<p>After ${numberOfRounds + (numberOfRounds == 1 ? " round" : " rounds")}, you won by a score of ${humanWins} to ${computerWins}.</p>`;
     }
     // ELSE IF computer is greater than  human
         // output that computer won
-    else if (computer > human){
-        console.log(`After 5 rounds, you lost by a score of ${human} to ${computer}`);
+    else if (winner === "computer"){
+        result.innerHTML += `<p>After ${numberOfRounds + (numberOfRounds == 1 ? " round" : " rounds")}, you lost by a score of ${humanWins} to ${computerWins}.</p>`;
     }
     // ELSE
         // tie game
     else {
-        console.log(`After 5 rounds, there was a tie of ${human} to ${computer}`)
+        result.innerHTML += `<p>After ${numberOfRounds + (numberOfRounds == 1 ? " round" : " rounds")}, there was a tie of ${humanWins} to ${computerWins}.</p>`;
     }
+}
+
+// start the game
+function startGame(){
+    resetGame();
+    // Get the number of rounds to play
+    numberOfRounds = document.querySelector("#numberOfRounds").value;
+    // enable buttons and start game with the provided number of rounds
+    changeInputAvailability();
+    changeRpsButtonsAvailability();
+    gameButton.textContent = `Reset`;
+}
+
+function resetGame(){
+    // reset rounds played back to 0
+    numberOfRoundsPlayed = 0;
+    // initialize human score to 0
+    humanWins = 0;
+    // initialize computer score to 0
+    computerWins = 0;
+    // change button to start game
+    gameButton.textContent = `Start`;
+    result.textContent = "";
 }
